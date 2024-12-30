f0e883c06497a5fecffc62bc974f4dd3d57768b3
Aaron Santiago <asantia727@gmail.com>
2024-12-30 16:06:26 -0500

    Add files via upload

---
+++ src/test/java/com/perscholas/GLAB/_95/__SP5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB._95.__SP5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
